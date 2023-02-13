255310f394de02c26f36bbfca001812f61c4e92f
harryb0905 <harryb0905@googlemail.com>
2023-02-13 16:06:35 +0000

    Minor CSS adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['17px', '28px'],
+      xl: ['19px', '32px'],
+      '2xl': ['24px']
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
