98a6f532151720c53f270451992cfceb4c49ff18
harryb0905 <harryb0905@googlemail.com>
2023-02-14 11:14:53 +0000

    Add more small screen responsivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontSize: {
+      xs: ['12px', '16px'],
       sm: ['14px', '20px'],
       base: ['16px', '24px'],
       lg: ['17px', '28px'],
       xl: ['19px', '32px'],
+      '1.5xl': ['22px'],
       '2xl': ['24px']
     },
     extend: {
